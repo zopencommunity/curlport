062aced68877ab0fdd9abaf0303991ca44ee6c21
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 09:41:30 -0400

    Add polling for changes in the jenkins script

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,23 @@
 node('linux') 
 {
+        stage ('Poll') {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/curlportport.git']]])
+                ])
+
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/utils.git']]])
+                ])
+        }
+
         stage('Build') {
                 build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'curlport'), string(name: 'DESCRIPTION', 'curl is used in command lines or scripts to transfer data.' )]
         }
